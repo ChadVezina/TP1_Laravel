47abfda4984861fe236277e101bc639dee26e6af
Chad Vezina <chadvezina@hotmail.com>
2025-10-05 08:54:53 -0400

    Créer un Seeder Ville pour remplir la table

---
+++ database/seeders/VilleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ville;
+use Illuminate\Database\Seeder;
+
+class VilleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $cities = ['Montreal', 'Quebec', 'Laval', 
+        'Gatineau', 'Longueuil', 'Sherbrooke', 
+        'Saguenay', 'Trois-Rivieres', 'Terrebonne', 
+        'Saint-Jean-sur-Richelieu'];
+
+        foreach ($cities as $city) {
+            Ville::create(['name' => $city]);
+        }
+    }
+}
