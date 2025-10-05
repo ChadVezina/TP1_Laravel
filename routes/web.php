73224adae7466bdbcb276cb9c06368c7e0ec1a3f
Chad Vezina <chadvezina@hotmail.com>
2025-10-05 10:02:30 -0400

    Premiere Route et Premier Layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EtudiantController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => view('welcome'));
+
+Route::resource('etudiants', EtudiantController::class);
